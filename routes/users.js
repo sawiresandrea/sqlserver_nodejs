8c727aa902f7d0798722fbea7729fcedd22d9ee7
sawiresandrea <sawires.andrea@iisgalvanimi.edu.it>
2020-01-08 10:34:59 +0000

    ms server sql installed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const sql = require('mssql')
+const config = {
+  user: '4DD_01',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_01', //(Nome del DB)
+}
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/attori', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select * from dbo.AA_ATTORE', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+  });
 });
 
 module.exports = router;
